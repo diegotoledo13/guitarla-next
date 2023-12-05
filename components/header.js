b75e369222de1c79a326b9a17b123a6d9f4dd2b7
Diego Toledo <diegomasteruno13@gmail.com>
2023-12-05 18:57:32 -0300

    haciendo footer y header

--- components/header.js
+++ components/header.js
@@ -1,21 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 export default function Header() {
+    const router = useRouter()
     return (
         <header className={styles.header}>
             <div className={`contenedor ${styles.barra}`}>
-                <Image src="/img/logo.svg"
-                    alt="Logotipo de GuitarraLA"
-                    width={250}
-                    height={40}
-                />
+                <Link href='/'>
+                    <Image src="/img/logo.svg"
+                        alt="Logotipo de GuitarraLA"
+                        width={250}
+                        height={40}
+                    />
+                </Link>
                 <nav className={styles.navegacion}>
-                    <Link href='/'>Inicio</Link>
-                    <Link href='/nosotros'>Nosotros</Link>
-                    <Link href='/blog'>Blog</Link>
-                    <Link href='/tienda'>Tienda</Link>
+                    <Link href='/' 
+                        className={ router.pathname === '/' ? styles.active : ''}>
+                            Inicio
+                    </Link>
+                    <Link href='/nosotros' 
+                        className={ router.pathname === '/nosotros' ? styles.active : ''}>
+                            Nosotros
+                    </Link>
+                    <Link href='/blog'
+                        className={ router.pathname === '/blog' ? styles.active : ''}>
+                            Blog
+                    </Link>
+                    <Link href='/tienda'
+                        className={ router.pathname === '/tienda' ? styles.active : ''}>
+                            Tienda
+                    </Link>
                 </nav>
             </div>
         </header>
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,29 @@
 import Layout from '../components/layout'
+import Image from 'next/image'
+import styles from '../styles/nosotros.module.css'
 export default function Nosotros() {
   return (
     <Layout
         title={'Nosotros'}
         description="Sobre nosotros, GuitarraLA, tienda de música">
-        <div>nosotros</div>
+        <main className='contenedor'>
+          <h2 className='heading'>Nosotros</h2>
+          <div>
+            <div className={styles.contenido}>
+              <Image src="/img/nosotros.jpg" width={1000} height={800} alt='Imagen sobre nosotros' />
+              <p>
+              ¡Bienvenido a GuitarraLA! En GuitarraLA, estamos apasionados por la música y comprometidos con proporcionar a nuestros clientes las mejores experiencias musicales a través de nuestras excepcionales guitarras. Desde 2023, nos hemos dedicado a ofrecer instrumentos de alta calidad y servicio excepcional, convirtiéndonos en un referente confiable para músicos de todos los niveles.
+              Lo que nos distingue es nuestra devoción por el arte de la guitarra. Ya sea que seas un principiante entusiasta o un músico experimentado, en GuitarraLA encontrarás una amplia variedad de guitarras que se adaptan a tus gustos y necesidades. Nos enorgullece colaborar con reconocidos fabricantes y artesanos para garantizar la excelencia en cada instrumento que ofrecemos.
+              Nuestra misión es más que vender guitarras; queremos inspirar a músicos a explorar nuevas melodías, a encontrar su propio sonido y a disfrutar del viaje musical. Nos esforzamos por crear un ambiente donde la pasión por la música se mezcla con la calidad de nuestros productos.
+              </p>
+              <p>En GuitarraLA, valoramos la satisfacción del cliente y nos esforzamos por superar tus expectativas. Nuestro equipo está formado por amantes de la música que están aquí para brindarte asesoramiento experto y ayudarte a encontrar la guitarra perfecta que se adapte a tu estilo.
+              Gracias por ser parte de nuestra comunidad musical. Explora nuestro catálogo, descubre la magia de nuestras guitarras y únete a nosotros en este viaje melódico. ¡Estamos aquí para hacer que tu experiencia musical sea inolvidable!
+              ¡Gracias por elegir GuitarraLA!
+              </p>
+
+            </div>
+          </div>
+        </main>
     </Layout>
   )
 }
