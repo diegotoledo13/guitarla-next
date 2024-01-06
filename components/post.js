275a6c6ef369b1d16bcae6bbd8e7b13f31c156c5
diegotoledo13 <diegomasteruno13@gmail.com>
2024-01-06 18:17:16 -0300

    trabajando en post con styles components

--- components/post.js
+++ components/post.js
@@ -1,29 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
+import { formatearFecha } from "@/utils/helpers";
+import styles from "../styles/blog.module.css";
 
 export default function Post({ post }) {
   const { contenido, imagen, titulo, url, publishedAt } = post;
   return (
     <article className="post">
-      <div className="imagen">
-        <Image
-          src={imagen.data.attributes.formats.medium.url}
-          alt={`Imagen blog ${titulo}`}
-          width={600}
-          height={400}
-        />
-      </div>
-      <div>
+      <Image
+        src={imagen.data.attributes.formats.medium.url}
+        alt={`Imagen blog ${titulo}`}
+        width={600}
+        height={400}
+      />
+      <div className={styles.contenido}>
         <h3>{titulo}</h3>
-
+        <p className={styles.fecha}>{formatearFecha(publishedAt)}</p>
         {contenido.map((parrafo, index) =>
           parrafo.children.map((child, childIndex) => (
-            <p key={`${index}-${childIndex}`}>{child.text}</p>
+            <p className={styles.resumen} key={`${index}-${childIndex}`}>
+              {child.text}
+            </p>
           ))
         )}
-
-        <p>{publishedAt}</p>
-        <Link href={`/blog/${url}`}>Leer más</Link>
+        <Link className={styles.enlace} href={`/blog/${url}`}>
+          Leer más
+        </Link>
       </div>
     </article>
   );
--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
 import Post from "../components/post";
+import styled from "../styles/grid.module.css";
 
 export default function Blog({ posts }) {
   return (
@@ -9,7 +10,7 @@ export default function Blog({ posts }) {
     >
       <main className="contenedor">
         <h1 className="heading">Blog</h1>
-        <div>
+        <div className={styled.grid}>
           {posts.map((post) => (
             <Post key={post.id} post={post.attributes} />
           ))}
