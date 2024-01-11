e3af1ae9331cbb4bb140819547653c85d878c8b3
diegotoledo13 <diegomasteruno13@gmail.com>
2024-01-11 18:38:42 -0300

    trabajando en carrito de compra .03

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,7 +1,8 @@
+import Image from "next/image";
 import Layout from "../components/layout";
 import style from "../styles/carrito.module.css";
 
-export default function Carrito() {
+export default function Carrito({ carrito, actualizarCantidad }) {
   return (
     <Layout>
       <main className="contenedor">
@@ -9,6 +10,47 @@ export default function Carrito() {
         <div className={style.contenido}>
           <div className={style.carrito}>
             <h2>Artículos</h2>
+            {carrito.length === 0 ? (
+              <p>No hay artículos</p>
+            ) : (
+              carrito.map((producto) => (
+                <div key={producto.id} className={style.producto}>
+                  <div>
+                    <Image
+                      width={250}
+                      height={480}
+                      src={producto.imagen}
+                      alt={producto.nombre}
+                    />
+                  </div>
+                  <div>
+                    <p className={style.nombre}>{producto.nombre}</p>
+                    <p>Cantidad : {producto.cantidad}</p>
+                    <p className={style.precio}>${producto.precio}</p>
+                    <div>
+                      <select
+                        className={style.select}
+                        onChange={(e) =>
+                          actualizarCantidad({
+                            id: producto.id,
+                            cantidad: e.target.value,
+                          })
+                        }
+                        value={producto.cantidad}
+                      >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </select>
+                    </div>
+                    <p className={style.subtotal}>Subtotal:</p>
+                    <span>${producto.cantidad * producto.precio}</span>
+                  </div>
+                </div>
+              ))
+            )}
           </div>
           <aside className={style.resumen}>
             <h3>Resumen del Pedido</h3>
