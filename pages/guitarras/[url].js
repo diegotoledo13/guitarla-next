0f90742bbf6c2b75262aef0a52186f508698e812
diegotoledo13 <diegomasteruno13@gmail.com>
2024-01-11 11:33:24 -0300

    trabajando en carrito de compra .02

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import Image from "next/image";
 import styled from "../../styles/guitarras.module.css";
 import Layout from "../../components/layout";
 import Link from "next/link";
 
-export default function Producto({ guitarra }) {
+export default function Producto({ guitarra, agregarCarrito }) {
+  const [cantidad, setCantidad] = useState(0);
+
   const { nombre, descripcion, imagen, precio } = guitarra[0].attributes;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (cantidad < 1) {
+      alert("Seleccione una cantidad");
+      return;
+    }
+    const guitarraSeleccionada = {
+      id: guitarra[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad,
+    };
+    agregarCarrito(guitarraSeleccionada);
+  };
   return (
     <Layout title={`Guitarra ${nombre}`}>
       <div className={styled.guitarra}>
@@ -20,6 +39,21 @@ export default function Producto({ guitarra }) {
             {descripcion[0].children[0].text}
           </p>
           <p className={styled.precio}>${precio}</p>
+          <form onSubmit={handleSubmit} className={styled.formulario}>
+            <label htmlFor="cantidad">Cantidad:</label>
+            <select
+              onChange={(e) => setCantidad(+e.target.value)}
+              id="cantidad"
+            >
+              <option value="0">--Seleccione--</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input type="submit" value="Agregar al carrito" />
+          </form>
         </div>
         <Link className={styled.enlace} href="/tienda">
           Volver al inicio
